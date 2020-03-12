07200dcfd53b77d1def9fc93acb3358099e3e256
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-12 14:04:20 -0600

    Add test to get fields by label text

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -15,3 +15,26 @@ test("first name, last name, email, and message exist on the form", () => {
   expect(email).toBeInTheDocument();
   expect(message).toBeInTheDocument();
 });
+
+test("simple data, no error messages", () => {
+  const { getByLabelText, fireEvent } = render(<ContactForm />);
+
+  const data = {
+    firstName: "billy",
+    lastName: "joel",
+    email: "billy.joel@gmail.com",
+    message: "We didn't start the fire"
+  };
+
+  const firstName = getByLabelText(/first name/i);
+  const lastName = getByLabelText(/last name/i);
+  const email = getByLabelText(/email/i);
+  const message = getByLabelText(/message/i);
+
+  //firstName.;
+
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+  expect(message).toBeInTheDocument();
+});
